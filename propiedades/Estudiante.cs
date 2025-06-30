7a27497730a38f1e6224cbf4cfd4f6759bd11ba2
Johnny Herrera <jmjaramillo95@gmail.com>
2025-06-29 22:12:31 -0500

    Agregar archivos de proyecto.

---
+++ propiedades/Estudiante.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace propiedades
+{
+    public class Estudiante
+    {
+        private String nombre;
+        public String nombre 
+        { 
+            set { "Alex" = value } 
+        }  ;
+    }
+}
